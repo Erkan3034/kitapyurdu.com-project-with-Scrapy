56cd3358d4d627196bed9fc0ac7a8686b936b0dd
Erkan3034 <turguterkan55@gmail.com>
2025-08-30 22:42:57 +0300

    Update QuotesSpider to write quotes to a new file, enhance output formatting, and implement pagination handling with error logging for missing pages

--- workAround/quotes_project/quotes_project/spiders/quotes_spyder.py
+++ workAround/quotes_project/quotes_project/spiders/quotes_spyder.py
@@ -27,7 +27,7 @@ class QuotesSpider(scrapy.Spider):
     # parse fonksiyonu
     def parse(self, response):
         quotes = response.css("div.quote")
-        with open("quotes.txt", "a", encoding="utf-8") as file:
+        with open("quotes_two.txt", "a", encoding="utf-8") as file:
             # her quote için
             for quote in quotes:
                 text = quote.css("span.text::text").get()
@@ -35,16 +35,25 @@ class QuotesSpider(scrapy.Spider):
                 tags = quote.css("div.tags a.tag::text").getall()
                 
                 # file.write(f"Text: {text}\n")
+                file.write(" ***************************** Quote: "+str(self.quote_count)+" *******************************\n")
                 file.write(f"Text: {text}\n")
                 file.write(f"Author: {author}\n")
-                file.write(f"Tags: {str(tags)}\n")
-                file.write("************************************************************\n")
+                file.write(f"Tags: {str(tags)}\n\n")
         
+                self.quote_count += 1
+
+            next_url = response.css("li.next a::attr(href)").get()
+            if next_url is not None:
+                next_url = "https://quotes.toscrape.com"+next_url
+                yield scrapy.Request(url=next_url, callback=self.parse) #yeni request oluştur
+            else:
+                print("ERROR: No next page found")
+"""     
         # Sonraki sayfaya git
         next_page = response.css('li.next a::attr(href)').get() 
         if next_page:
             yield response.follow(next_page, self.parse) # next page'i takip et
-
+"""   
 
 
 # run the spider()
