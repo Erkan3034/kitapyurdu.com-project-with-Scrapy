b70ba29b65a12cc9d9f4d3d7deb4eba2434045a9
Erkan3034 <turguterkan55@gmail.com>
2025-08-30 22:12:56 +0300

    Enhance QuotesSpider: Add allowed_domains, update start_urls with comments, and improve parsing logic with additional comments for clarity

--- workAround/quotes_project/quotes_project/spiders/quotes_spyder.py
+++ workAround/quotes_project/quotes_project/spiders/quotes_spyder.py
@@ -5,14 +5,17 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes" # spider name
+    quote_count = 1
+    allowed_domains = ["quotes.toscrape.com"]
 
+    # gidilecek sayfalar
     start_urls = [
         "https://quotes.toscrape.com/page/1/",
-        "https://quotes.toscrape.com/page/2/",
+        # "https://quotes.toscrape.com/page/2/", (link takip etmek için bunu yoruma alıp quote__count ile sayfa sayısını bulabiliriz)
     ]
 
 
-
+    # 2. yol
     """def start_requests(self):
         urls = [
             "https://quotes.toscrape.com/page/1/",
@@ -21,14 +24,17 @@ class QuotesSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
     """
+    # parse fonksiyonu
     def parse(self, response):
         quotes = response.css("div.quote")
         with open("quotes.txt", "a", encoding="utf-8") as file:
+            # her quote için
             for quote in quotes:
                 text = quote.css("span.text::text").get()
                 author = quote.css("small.author::text").get()
                 tags = quote.css("div.tags a.tag::text").getall()
                 
+                # file.write(f"Text: {text}\n")
                 file.write(f"Text: {text}\n")
                 file.write(f"Author: {author}\n")
                 file.write(f"Tags: {str(tags)}\n")
