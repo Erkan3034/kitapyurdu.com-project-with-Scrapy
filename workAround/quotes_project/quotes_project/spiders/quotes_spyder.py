a53fd483158856011d9d41c73e234a50946a0174
Erkan3034 <turguterkan55@gmail.com>
2025-08-30 23:00:04 +0300

    Refactor QuotesSpider to manage file operations more efficiently, ensuring quotes are appended correctly and file is closed properly after scraping. Enhance quote parsing logic and maintain pagination functionality.

--- workAround/quotes_project/quotes_project/spiders/quotes_spyder.py
+++ workAround/quotes_project/quotes_project/spiders/quotes_spyder.py
@@ -4,8 +4,9 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes" # spider name
+    name = "quotes"  # spider name
     quote_count = 1
+    file = open("quotes_two.txt", "a", encoding="utf-8")
     allowed_domains = ["quotes.toscrape.com"]
 
     # gidilecek sayfalar
@@ -14,46 +15,30 @@ class QuotesSpider(scrapy.Spider):
         # "https://quotes.toscrape.com/page/2/", (link takip etmek için bunu yoruma alıp quote__count ile sayfa sayısını bulabiliriz)
     ]
 
+    def open_spider(self, spider):
+        # Dosyayı spider başında aç
+        self.file = open("quotes_two.txt", "a", encoding="utf-8")
 
-    # 2. yol
-    """def start_requests(self):
-        urls = [
-            "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-    """
-    # parse fonksiyonu
+    def close_spider(self, spider):
+        # Dosyayı spider sonunda kapat
+        if hasattr(self, "file") and not self.file.closed:
+            self.file.close()
+
     def parse(self, response):
         quotes = response.css("div.quote")
-        with open("quotes_two.txt", "a", encoding="utf-8") as file:
-            # her quote için
-            for quote in quotes:
-                text = quote.css("span.text::text").get()
-                author = quote.css("small.author::text").get()
-                tags = quote.css("div.tags a.tag::text").getall()
-                
-                # file.write(f"Text: {text}\n")
-                file.write(" ***************************** Quote: "+str(self.quote_count)+" *******************************\n")
-                file.write(f"Text: {text}\n")
-                file.write(f"Author: {author}\n")
-                file.write(f"Tags: {str(tags)}\n\n")
-        
-                self.quote_count += 1
+        for quote in quotes:
+            text = quote.css("span.text::text").get()
+            author = quote.css("small.author::text").get()
+            tags = quote.css("div.tags a.tag::text").getall()
 
-            next_url = response.css("li.next a::attr(href)").get()
-            if next_url is not None:
-                next_url = "https://quotes.toscrape.com"+next_url
-                yield scrapy.Request(url=next_url, callback=self.parse) #yeni request oluştur
-            else:
-                print("ERROR: No next page found")
-"""     
-        # Sonraki sayfaya git
-        next_page = response.css('li.next a::attr(href)').get() 
+            self.file.write(" ***************************** Quote: " + str(self.quote_count) + " *******************************\n")
+            self.file.write(f"Text: {text}\n")
+            self.file.write(f"Author: {author}\n")
+            self.file.write(f"Tags: {str(tags)}\n\n")
+            self.quote_count += 1
+
+        # Sonraki sayfaya gitmek için next_page linkini bul ve takip et
+        next_page = response.css('li.next a::attr(href)').get()
         if next_page:
-            yield response.follow(next_page, self.parse) # next page'i takip et
-"""   
-
-
-# run the spider()
+            next_url = "https://quotes.toscrape.com"+next_page
+            yield scrapy.Request(url=next_url, callback=self.parse)
